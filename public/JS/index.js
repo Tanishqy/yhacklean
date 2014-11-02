17ca886e7d4681544e46ac8936bd03a1edb77783
Tanishqy <tanishqsuryavanshi@gmail.com>
2014-11-02 00:27:23 -0400

    use this plz

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,17 +1,23 @@
-function NotesCtrl($scope){
-    	$scope.title="How did you feel today?";
-
-    	$scope.notes=[];
-
-    	$scope.insert=function(note) {
-    		var n = angular.copy(note);
-    		n.createdAt = new Date()
-    		$scope.notes.push(n);
-    		note.content = "";
-
-    	}
-    }
-
-
-
+var yhack = angular.module('yhack', ["ui.router"])
+yhack.config(function($stateProvider, $urlRouteProvider) {
     
+    $urlRouteProvider.otherwise("/questions")
+    
+    $stateProvider
+        .state('questions', {
+            url= "/questions",
+            templateURL = "partials/questions.html"
+        })
+        
+        .state('statistics', {
+            url= "/statistics",
+            templateURL = "partials/statistics.html"
+        })
+               
+               
+               
+               
+               
+function numbgenerator() {
+        var randomnumber = Math.floor(Math.random()*11)
+        }
